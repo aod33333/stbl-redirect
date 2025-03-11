3ce4ae8572de26cc0187c9866ea2bdd5285f3c8d
aod33333 <alijohnod@gmail.com>
2025-03-11 02:13:53 +0000

    Add files via upload

---
+++ redirect.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+import json
+import base64
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add_to_wallet():
+    encoded_data = request.args.get('data')
+    data = json.loads(base64.b64decode(encoded_data).decode('utf-8'))
+    script = f"""
+    <script>
+    if (window.ethereum) {{
+      window.ethereum.request({json.dumps({"method": "wallet_addEthereumChain", "params": [data["chain"]]})})
+        .then(() => window.ethereum.request({json.dumps({"method": "wallet_watchAsset", "params": data["token"]})}))
+        .catch(err => alert('Error: ' + err.message));
+    }} else {{
+      alert('Please install MetaMask!');
+    }}
+    </script>
+    """
+    return f"<html><body>{script}</body></html>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=False)
